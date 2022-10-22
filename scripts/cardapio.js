dde0a34b559a7f69af6244ac2191eec107a2ef23
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2022-10-22 16:34:56 -0300

    pop up for expresso done

--- scripts/cardapio.js
+++ scripts/cardapio.js
@@ -1,1 +1,17 @@
-var cu = "cu"
+const minus = document.querySelector('.minus-btn');
+const plus = document.querySelector('.plus-btn');
+const counter = document.querySelector('.counter-text');
+
+let a = 0;
+
+minus.addEventListener('click', () => {
+    if (a > 0) {
+        a--;
+        counter.innerHTML = a;
+    }
+});
+
+plus.addEventListener('click', () => {
+    a++;
+    counter.innerHTML = a;
+});
