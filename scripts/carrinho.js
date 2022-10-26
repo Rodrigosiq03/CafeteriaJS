de4f409a9643c93180d3facf9abafc82e58eaee1
Lucas Duez <46378322+Lucasdvs10@users.noreply.github.com>
2022-10-26 09:23:00 -0300

    pegando todos os itens do local storage e renderizando-os na tela de carrinho

--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -1,7 +1,37 @@
-const itens = document.getElementById('itens-pop-up');
-console.log(itens);
+const itens = document.getElementById("itens-pop-up");
+const produtosCardTemplate = document.getElementById("card-produto-template");
+const listaProdutos = document.getElementById("lista-produtos-carrinho");
 
 function itensPopUp() {
-    let pop_up_itens = new bootstrap.Modal(itens);
-    pop_up_itens.show();
+  let pop_up_itens = new bootstrap.Modal(itens);
+  pop_up_itens.show();
 }
+
+function getAllItensFromLocalStorage() {
+  let itensList = [];
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let currentKey = localStorage.key(i);
+    itensList.push(JSON.parse(localStorage.getItem(currentKey)));
+  }
+  return itensList;
+}
+
+function showProductsOnCart() {
+  let allItens = getAllItensFromLocalStorage();
+
+  allItens.forEach((item) => {
+    let li = document.createElement("li");
+    listaProdutos.appendChild(li);
+
+    let templateCopy = produtosCardTemplate.content.cloneNode(true)
+    
+    templateCopy.querySelector(".nome-produto").textContent = item["name"]
+    templateCopy.querySelector(".preco-produto").textContent = item["price"]
+    templateCopy.querySelector(".img-produto").src = item["src"]
+
+    li.appendChild(templateCopy)
+  });
+}
+
+showProductsOnCart()
